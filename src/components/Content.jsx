d84f1150d5f234d6d63a9dda68ec6ce4d7c048e0
Dulana Nisayuru <IT21817762@my.sliit.lk>
2025-06-20 14:59:51 +0530

    images add

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,7 @@
-import Navbar from "./Navbar";
 import './css/Content.css';
 import searchIcon from '../Assets/Icons/Search White.svg';
+import movieImg1 from '../Assets/Images/Wild West.jpg';
+import movieImg2 from '../Assets/Images/Batman.jpg';
 const Content = () => {
     
     return ( 
@@ -27,7 +28,28 @@ const Content = () => {
 
                 <div className="movie-thumb">
                     <div className="movies">
-                        
+                        <img src={movieImg1} alt="movieImg1" className='movie-images'/>
+                        <h3 className='movie-name'>Batman Returns</h3>
+                        <p3 className='movie-desc'>Lorem ipsum dolor sit amet,
+                            consetetur sadipscing elitr, sed diam
+                            nonumy eirmod tempor invidunt ut…
+                        </p3>
+                    </div>
+                    <div className="movies">
+                        <img src={movieImg1} alt="movieImg1" className='movie-images'/>
+                        <h3 className='movie-name'>Wild Wild West</h3>
+                        <p3 className='movie-desc'>Lorem ipsum dolor sit amet,
+                            consetetur sadipscing elitr, sed diam
+                            nonumy eirmod tempor invidunt ut…
+                        </p3>
+                    </div>
+                    <div className="movies">
+                        <img src={movieImg1} alt="movieImg1" className='movie-images'/>
+                        <h3 className='movie-name'>The Amazing Spiderman</h3>
+                        <p3 className='movie-desc'>Lorem ipsum dolor sit amet,
+                            consetetur sadipscing elitr, sed diam
+                            nonumy eirmod tempor invidunt ut…
+                        </p3>
                     </div>
                 </div>
             </div> 
