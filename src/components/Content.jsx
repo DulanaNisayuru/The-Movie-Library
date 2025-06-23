0079d4b409bd933410cfe35e279bb8f325cc952c
Dulana Nisayuru <IT21817762@my.sliit.lk>
2025-06-23 12:48:34 +0530

    form start

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,6 +2,7 @@ import './css/Content.css';
 import searchIcon from '../Assets/Icons/Search White.svg';
 import movieImg1 from '../Assets/Images/Wild West.jpg';
 import movieImg2 from '../Assets/Images/Batman.jpg';
+import movieImg3 from '../Assets/Images/Spiderman.jpg'
 const Content = () => {
     
     return ( 
@@ -31,25 +32,27 @@ const Content = () => {
                 <div className="movie-thumb">                
 
                     <div className="movies">
-                        <img src={movieImg1} alt="movieImg1" className='movie-images'/>
-                        <h3 className='movie-name'>Batman Returns</h3>
-                        <p3 className='movie-desc'>Lorem ipsum dolor sit amet,
-                            consetetur sadipscing elitr, sed diam
-                            nonumy eirmod tempor invidunt ut…
-                        </p3>
-                    </div>
-                    <div className="movies">
-                        <img src={movieImg1} alt="movieImg1" className='movie-images'/>
+                        <img src={movieImg2} alt="movieImg1" className='movie-images'/>
                         <div className="description">
-                        <h3 className='movie-name'>Wild Wild West</h3>
-                        <p3 className='movie-desc'>Lorem ipsum dolor sit amet,
-                            consetetur sadipscing elitr, sed diam
-                            nonumy eirmod tempor invidunt ut…
-                        </p3>
+                            <h3 className='movie-name'>Batman Returns</h3>
+                            <p3 className='movie-desc'>Lorem ipsum dolor sit amet,
+                                consetetur sadipscing elitr, sed diam
+                                nonumy eirmod tempor invidunt ut…
+                            </p3>
                         </div>
                     </div>
                     <div className="movies">
                         <img src={movieImg1} alt="movieImg1" className='movie-images'/>
+                        <div className="description">
+                            <h3 className='movie-name'>Wild Wild West</h3>
+                            <p3 className='movie-desc'>Lorem ipsum dolor sit amet,
+                                consetetur sadipscing elitr, sed diam
+                                nonumy eirmod tempor invidunt ut…
+                            </p3>
+                        </div>
+                    </div>
+                    <div className="movies">
+                        <img src={movieImg3} alt="movieImg1" className='movie-images'/>
                         <div className="description">
                             <h3 className='movie-name'>The Amazing Spiderman</h3>
                             <p3 className='movie-desc'>Lorem ipsum dolor sit amet,
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,73 @@
+import React,{useState} from "react";
+import './css/Footer.css';
+
 const Footer = () => {
+    const [formData, setFormData] = useState({
+        firstName:'',
+        lastName:'',
+        email:'',
+        telephone:'',
+        message:'',
+        agree:'false'
+    });
+
+    const handleChange = (e) =>{
+        const {name, value} = e.target;
+        setFormData((prev) =>({
+            ...prev,
+            [name]:value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("form submitted...",formData);
+    }
+
     return ( 
         <div className="footer">
-            
+            <div className="footer-head">
+                <h2>How to reach us</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consetetur.
+                </p>
+            </div>
+
+            <div className="footer-middle">
+                <form onSubmit={handleSubmit} className="contact-form">
+                    <div className="name-fields">
+                        <label>First Name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            value={formData.firstName}
+                            onChange={handleChange}
+                            required
+                        />
+
+                        <label>Last Name</label>
+                        <input
+                            type="text"
+                            name="lastName"
+                            value={formData.lastName}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                </form>
+
+                <iframe
+                    title="Google Map"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48588.133010545425!2d-3.7152472!3d40.4363503!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422f35d4f0c269%3A0x754405946e8ca584!2sAmadeus%20IT%20Group!5e0!3m2!1sen!2slk!4v1750428594601!5m2!1sen!2slk"
+                    width="100%"
+                    height="400"
+                    style={{ border: 0 }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade">     
+                </iframe>
+                
+            </div>
         </div>
      );
 }
